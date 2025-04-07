d87f3a73ae43d7030176b8cdfb2df304df743374
VedadBukva <vedadbukva@gmail.com>
2025-04-07 14:39:43 +0200

    Completed responsive layout

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,14 +10,14 @@ function Contact() {
         <div className="contact-wrapper">
             <div className="contact-maps">
                 <div className="contact-map">
-                    <div>
+                    <div className="contact-map-bakeries">
                         <p>"Sito" pekare</p>
                         <p>Sarajevo</p>
                         <iframe className="contact-map-iframe" src="https://www.google.com/maps/embed?pb=!1m12!1m8!1m3!1d54659.120958473155!2d18.3447647!3d43.8600601!3m2!1i1024!2i768!4f13.1!2m1!1ssito%20pekare!5e1!3m2!1sen!2sba!4v1743937558824!5m2!1sen!2sba" width="500" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>        
                     </div>
                 </div>
                 <div className="contact-manifacture">
-                    <div>
+                <div className="contact-map-manifacture">
                         <p>"Sitortilla" proizvodnja</p>
                         <p>Goražde</p>
                         <iframe className="contact-map-iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3331.244542452509!2d18.972896602851257!3d43.67116232208135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47588b003597b2e3%3A0xd77a932a8a793569!2sSito%20Gora%C5%BEde!5e1!3m2!1sen!2sba!4v1743942844123!5m2!1sen!2sba" width="500" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>        
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -37,8 +37,8 @@ const Services = () => {
       <div className="services-wrapper">
         <div id="sitortilla"></div>
         <div className="services-sitortilla">
-            <div className="services-image">
-                <iframe width="350" height="300" src="https://www.youtube.com/embed/wFmVT14j1Q4?si=hv-Gb4FjoPFH4TEi?autoplay=1" title="YouTube video player" frameborder="0" allow="autoplay; encrypted-media;" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+            <div className="services-image services-sitortilla-video">
+                <iframe className='services-sitortilla-iframe' width="350" height="300" src="https://www.youtube.com/embed/wFmVT14j1Q4?si=hv-Gb4FjoPFH4TEi?autoplay=1" title="YouTube video player" frameborder="0" allow="autoplay; encrypted-media;" referrerpolicy="strict-origin-when-cross-origin"></iframe>
             </div>
             <div className="services-text">
                 <h1>Sitortilla</h1>
